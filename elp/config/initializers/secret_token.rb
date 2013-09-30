6870c810a73f2cb592965b8372eb63fc32f6e3c6
Tom Andrews <git@tomandrews.co.uk>
2013-09-30 22:01:26 +0100

    Just created rails project

---
+++ elp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Elp::Application.config.secret_key_base = '1d3857678257c9555061117ac440edc80651e3a8dab3571c22656893e7aebdc4df3f4e3d69c2cf5a436249adfaaaa22cd267ab006721fb432f6254b22d786edd'
---
+++ elp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Elp::Application.config.session_store :cookie_store, key: '_elp_session'
